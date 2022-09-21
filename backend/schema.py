ed4776930306d4fb6f0d7e86ba73a145d660eeb4
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-09-21 14:00:22 +0200

    Schema for user and todo

---
+++ backend/schema.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+    phone_number: str
+
+
+class Todo(BaseModel):
+    title: str = Field(min_length=1)
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
