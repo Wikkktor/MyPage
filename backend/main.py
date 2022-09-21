8bd699ed894176202c6601e6ad81875157fd378d
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2022-09-21 13:08:55 +0200

    Adding template view

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,31 @@
-from pathlib import Path
-from fastapi import FastAPI, APIRouter, Request, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, APIRouter, Request
+from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import models
-from database import engine, get_db
+from database import engine
 
 root_router = APIRouter()
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+TEMPLATES = Jinja2Templates(directory="templates")
 
 
 @app.get('/', status_code=200)
 async def test(
-        db: Session = Depends(get_db)
-):
-    return  db.query(models.Todos).all()
+        request: Request
+        ):
+    """
+    Root get
+    """
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
