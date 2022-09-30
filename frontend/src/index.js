e878ddad3c4667b31daccf07ad7e0bb0129da0c6
Wikkktor <wkaraszewicz@gmail.com>
2022-09-30 14:06:11 +0200

    Including user context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import 'bulma/css/bulma.min.css';
 import App from './App';
-
+import { UserProvider} from "./context/UserContext";
 
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+    <UserProvider>
+            <App tab="home" />
+    </UserProvider>
+
+);
