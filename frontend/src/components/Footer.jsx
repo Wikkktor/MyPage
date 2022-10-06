081474cc6f70cf0c919b21684e916bc4a0a6b0d0
Wikkktor <wkaraszewicz@gmail.com>
2022-10-06 11:17:52 +0200

    footer creation

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer">
+                <div className="content has-text-centered">
+                    <p>
+                        <strong>Wiktor Karaszewicz Portfolio</strong> The source code is
+                        on github <a href="http://github.com/wikkktor/mypage">Check</a>.
+                    </p>
+                </div>
+            </footer>
+        </>
+    )
+}
+export default Footer
