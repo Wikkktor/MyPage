8da4cb13cf89b32941e64513a40330b8dcb5f5a8
Wikkktor <wkaraszewicz@gmail.com>
2022-10-06 23:12:04 +0200

    adding style to message

--- frontend/src/components/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ErrorMessage = ({message}) => (
-    <p className="has-text-weight-bold has-text-danger">{message}</p>
+    <p style={{fontWeight: "600", color: "#d90429", fontSize: "18px"}}>{message}</p>
 )
 
 export default ErrorMessage
