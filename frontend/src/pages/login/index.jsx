d82e3e727978b06cc94c9a0d0a321207739acdcc
Wikkktor <wkaraszewicz@gmail.com>
2022-10-07 10:38:29 +0200

    Redirect after login and logged in users

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,13 +1,14 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {UserContext} from "../../context/UserContext";
 import ErrorMessage from "../../components/ErrorMessage";
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
-    const [, setToken] = useContext(UserContext);
+    const [token, setToken] = useContext(UserContext);
+    const navigate = useNavigate();
 
     const submitLogin = async () => {
         const requestOptions = {
@@ -24,6 +25,7 @@ const Login = () => {
             setErrorMessage(data.detail)
         } else {
             setToken(data.access_token)
+            navigate("/")
         }
     };
 
@@ -31,52 +33,56 @@ const Login = () => {
         e.preventDefault()
         submitLogin()
     }
+    useEffect(() => {
+        if (token){
+            navigate("/")
+        }
+    })
 
-    return (
-        <>
-            <div className='ui container' style={{marginTop: "30vh"}}>
-                <div className="ui grid stackable">
-                    <div className='three wide column'></div>
-                    <div className="ten wide column">
-                        <form className="ui form"  onSubmit={handleSubmit}>
-                            <h1 style={{textAlign:"center"}} className="ui header">Login</h1>
-                            <div className="field">
-                                <label className="label">Username</label>
-                                <div className="control">
-                                    <input
-                                        type='text'
-                                        placeholder='Enter your username'
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        className="input"
-                                        required
-                                    />
+        return (
+            <>
+                <div className='ui container' style={{marginTop: "30vh"}}>
+                    <div className="ui grid stackable">
+                        <div className='three wide column'></div>
+                        <div className="ten wide column">
+                            <form className="ui form" onSubmit={handleSubmit}>
+                                <h1 style={{textAlign: "center"}} className="ui header">Login</h1>
+                                <div className="field">
+                                    <label className="label">Username</label>
+                                    <div className="control">
+                                        <input
+                                            type='text'
+                                            placeholder='Enter your username'
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            className="input"
+                                            required
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="field">
-                                <label className="label">Password</label>
-                                <div className="control">
-                                    <input
-                                        type='password'
-                                        placeholder='Enter your username'
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        className="input"
-                                        required
-                                    />
+                                <div className="field">
+                                    <label className="label">Password</label>
+                                    <div className="control">
+                                        <input
+                                            type='password'
+                                            placeholder='Enter your username'
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            className="input"
+                                            required
+                                        />
+                                    </div>
+                                    <ErrorMessage message={errorMessage}/>
                                 </div>
-                                <ErrorMessage message={errorMessage}/>
-                            </div>
-                            <div style={{width: '100%', textAlign: 'center'}}>
-                            <button className="ui button" type="submit">Login</button>
-                            </div>
-                        </form>
+                                <div style={{width: '100%', textAlign: 'center'}}>
+                                    <button className="ui button" type="submit">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className='three wide column'></div>
                     </div>
-                    <div className='three wide column'></div>
                 </div>
-            </div>
-        </>
-    )
-}
-
+            </>
+        )
+    }
 export default Login
