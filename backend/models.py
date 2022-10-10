bec37be8e60fbbbe85248867772a65dc564d4aca
Wikkktor <wkaraszewicz@gmail.com>
2022-10-10 11:31:51 +0200

    upgrading models to actual database

--- backend/models.py
+++ backend/models.py
@@ -7,15 +7,11 @@ class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
     username = Column(String, unique=True, index=True)
-    first_name = Column(String)
-    last_name = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    phone_number = Column(String)
 
-    todos = relationship("Todos", back_populates="owner")
+    todos = relationship("Todos", cascade='all,delete', back_populates="owner", passive_deletes=True)
 
 
 class Todos(Base):
@@ -26,6 +22,6 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'))
 
     owner = relationship("Users", back_populates="todos")
