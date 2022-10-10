e185db5a937cb496d89afaf3dcb10c71d9fceb54
Wikkktor <wkaraszewicz@gmail.com>
2022-10-10 10:40:57 +0200

    Adding links to navbar for mobiles

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,19 +5,17 @@ const Navbar = () => {
     return (
         <>
             <div className="ui vertical inverted sidebar menu">
-                <div>
-                    <Link to='/' className="active item">Main page</Link>
-                    <a href='/#about_me' className='item'>About me</a>
-                    <a href='/#experience' className='item'>Experience</a>
-                    <a href='/#portfolio' className='item'>Portfolio</a>
-                    <a href='/#education' className='item'>Education</a>
-                    <a href='/#skills' className='item'>Skills</a>
-                    <a href='/#skills' className='item'>Contact</a>
-                </div>
-                <div>
-                    <Link to='/login' className="item">Login</Link>
-                    <Link to='/register' className="item">Signup</Link>
-                </div>
+                <Link to='/' className="active item">Main page</Link>
+                <a href='/#about_me' className='item'>About me</a>
+                <a href='/#experience' className='item'>Experience</a>
+                <a href='/#portfolio' className='item'>Portfolio</a>
+                <a href='/#education' className='item'>Education</a>
+                <a href='/#skills' className='item'>Skills</a>
+                <a href='/#skills' className='item'>Contact</a>
+                <Link to='/todo' className='item'>Todo App</Link>
+                <Link to='/jokes' className='item'>Jokes App</Link>
+                <Link to='/login' className="item">Login</Link>
+                <Link to='/register' className="item">Signup</Link>
             </div>
             <Outlet/>
 
