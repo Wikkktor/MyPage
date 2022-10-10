4c7b975f787861229b43430a27e38889131b8c81
Wikkktor <wkaraszewicz@gmail.com>
2022-10-10 21:29:51 +0200

    Adding delete todo

---
+++ frontend/src/components/DeleteTask.jsx
@@ -0,0 +1,25 @@
+import React, {useContext} from "react";
+import {UserContext} from "../context/UserContext";
+
+const DeleteTask = (task) => {
+    const [token] = useContext(UserContext);
+
+    async function delete_task() {
+        const requestOptions = {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+        };
+        await fetch(`/api/todos/${task.task}`, requestOptions);
+
+    }
+
+    return (
+        <>
+            <i style={{color: 'red', cursor: 'pointer'}} className="trash alternate icon" onClick={delete_task}></i>
+        </>
+    )
+}
+
+export default DeleteTask
