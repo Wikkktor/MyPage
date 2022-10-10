e4ab7a7ec5ccfe6e2413c5a15fbc0585ef520819
Wikkktor <wkaraszewicz@gmail.com>
2022-10-10 21:29:15 +0200

    adding route to todoapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./pages/home";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
+import TodoPage from "./pages/todo"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
@@ -13,6 +14,7 @@ const App = () => {
                 <Route index element={<Home/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path='/register' element={<RegisterPage/>}/>
+                <Route path='/todo' element={<TodoPage/>}/>
             </Routes>
         </BrowserRouter>
     )
