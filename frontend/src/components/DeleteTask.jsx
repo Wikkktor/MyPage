5e025e86c0c6a71ea1170331d81219483c821026
Wikkktor <wkaraszewicz@gmail.com>
2022-10-11 16:29:34 +0200

    Adding id to passed object

--- frontend/src/components/DeleteTask.jsx
+++ frontend/src/components/DeleteTask.jsx
@@ -11,7 +11,7 @@ const DeleteTask = (task) => {
                 Authorization: "Bearer " + token,
             },
         };
-        await fetch(`/api/todos/${task.task}`, requestOptions);
+        await fetch(`/api/todos/${task.task.id}`, requestOptions);
 
     }
 
