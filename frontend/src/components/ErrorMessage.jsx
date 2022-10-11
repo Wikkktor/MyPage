3df734d607f15c405924bc7fc24978cc153a6c3c
Wikkktor <wkaraszewicz@gmail.com>
2022-10-11 16:30:49 +0200

    error message font size'

--- frontend/src/components/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ErrorMessage = ({message}) => (
-    <p style={{fontWeight: "600", color: "#d90429", fontSize: "18px"}}>{message}</p>
+    <p style={{fontWeight: "600", color: "#d90429", fontSize: "16px"}}>{message}</p>
 )
 
 export default ErrorMessage
