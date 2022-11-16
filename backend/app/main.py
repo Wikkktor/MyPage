c58a1ed6ab9d59f107d1725103aa3bb4b435ca1b
Wikkktor <wkaraszewicz@gmail.com>
2022-11-16 17:54:50 +0100

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Request, File, UploadFile, Form
 from fastapi.templating import Jinja2Templates
 from app import models
+from typing import Dict, Any
 from app.database import engine
 from app.routers import auth, todos, users
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from passwords import conf
+from fastapi.responses import JSONResponse
+from PIL import Image
+import io
 
 root_router = APIRouter()
 app = FastAPI()
@@ -33,8 +39,68 @@ async def test(
     """
     Root get
     """
-    return {"message": "Test success"}
+    return {"message": "Test success!"}
 
+
+@app.post('/email-grupalucrum', status_code=200)
+async def email(data: Dict[Any, Any]):
+    section1 = ''
+    for key, val in data['formInputs']['section1'].items():
+        if val:
+            section1 += f"{key} <br>"
+    
+    section2 = ''
+    for key, val in data['formInputs']['section3'].items():
+        section2 += f"{key} - {val} <br>" 
+    
+    msg = f"""
+    <h2>Wybrane typy:</h2> <br>
+    {section1}
+    <br>
+    Zakres budżetu: {data['formInputs']['rangeAmount']} <br>
+    <br>
+    <h3>DANE:</h3> <br>
+    {section2}
+    """
+    message = MessageSchema(
+        subject="Grupa Lucrum Wiadomość",
+        recipients=["kontakt@grupalucrum.pl",],
+        body=msg,
+        subtype=MessageType.html)
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"}) 
+
+
+
+@app.post('/email-kariera', status_code=200)
+async def sendCV(
+    name: str = Form(...),
+    lastname: str = Form(...), 
+    email: str = Form(...), 
+    message: str = Form(...), 
+    file: UploadFile = File(...)):
+
+    msg = f"""
+    <h1>Ktoś przesłał CV</h1> 
+    <br>
+    Imię: {name} <br>
+    Naziwsko: {lastname} <br>
+    Email: {email} <br>
+    Wiadomość: {message}
+    """
+
+    message = MessageSchema(
+        subject="Grupa Lucrum Wiadomość",
+        recipients=["kontakt@grupalucrum.pl",],
+        body=msg,
+        subtype=MessageType.html,
+        attachments=[file,])
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"message": "email has been sent"}) 
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
